e2b744b13429f00b110e89faf3d0d2c6d1345436
Nikolay Mayorov <slidings@mail.ru>
2020-12-06 17:58:16 +0300

    Клиенские библиотеки

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -7,34 +7,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller //ControllerBase  для веб апи
     {
-        private static readonly List<Employee> employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                FirstName = "Ivan",
-                SurName = "Ivanov",
-                Patronymic = "Ivanovich",
-                Age = 54
-            },
-            new Employee
-            {
-                Id = 2,
-                FirstName = "Sergey",
-                SurName = "Petrov",
-                Patronymic = "Bogdanovich",
-                Age = 28
-            },
-            new Employee
-            {
-                Id = 3,
-                FirstName = "Boris",
-                SurName = "Grozniy",
-                Patronymic = "Ivanovich",
-                Age = 19
-            }
-        };
-
         public IActionResult Error404() => View();
 
         public IActionResult Blog() => View();
@@ -57,19 +29,6 @@ namespace WebStore.Controllers
         {
             return View();
             // return Content("Home controller");
-        }
-
-        public IActionResult Employee()
-        {
-            return View(model: employees);
-        }
-
-        public IActionResult EmployeeData(int id)
-        {
-            var emp = employees.FirstOrDefault(e => e.Id == id);
-            if (emp is null)
-                return NotFound();
-            return View(model: emp);
         }
     }
 }
