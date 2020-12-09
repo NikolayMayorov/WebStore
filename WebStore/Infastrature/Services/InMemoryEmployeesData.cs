12ac088f78d6dc3392a6cf7677756e90f722e560
Nikolay Mayorov <slidings@mail.ru>
2020-12-09 20:48:29 +0300

    Редактирование сотрудников

---
+++ WebStore/Infastrature/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Infastrature.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infastrature.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            return TestData.Employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            return TestData.Employees.FirstOrDefault(emp => emp.Id == id);
+        }
+
+        public void Add(Employee emp)
+        {
+            if (emp is null)
+                throw new ArgumentNullException(nameof(emp));
+            emp.Id = TestData.Employees.Max(e => e.Id) + 1;
+            TestData.Employees.Add(emp);
+        }
+
+        public void Edit(int id, Employee emp)
+        {
+            if (emp is null)
+                throw new ArgumentNullException(nameof(emp));
+            var db_emp = GetById(id);
+            if (db_emp is null)
+                return;
+            db_emp.SurName = emp.SurName;
+            db_emp.Patronymic = emp.Patronymic;
+            db_emp.Age = emp.Age;
+            db_emp.FirstName = emp.FirstName;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_emp = GetById(id);
+            if (db_emp is null)
+                return false;
+            var result = TestData.Employees.Remove(db_emp);
+            return result;
+        }
+
+        public void SaveChanges()
+        {
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,61 +1,63 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-
+using WebStore.Infastrature.Interfaces;
+using WebStore.Infastrature.Services;
 
 namespace WebStore
 {
     public class Startup
     {
-        private IConfiguration Configuration { get;}
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
+        private IConfiguration Configuration { get; }
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          //  services.AddMvc(); // core 2.0
-          services.AddControllersWithViews();
+            //  services.AddMvc(); // core 2.0
+            services.AddControllersWithViews();
 
+
+            //services.AddTransient  создается каждый раз свой экземпляр
+            //services.AddScoped - один экземаля на область видимости
+
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseWelcomePage("/welcome");
 
             app.UseStaticFiles();
             app.UseDefaultFiles(); //!!!!!!
 
             app.UseRouting();
 
-        
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/helloworld", async context =>
-                {
-                    await context.Response.WriteAsync(Configuration.GetSection("Gretigns").Value);
-                });
+                endpoints.MapGet("/helloworld",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync(text: Configuration.GetSection("Gretigns").Value);
+                    });
+
 
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
